0b0d091a4e62dfa17ac60308ca7cadeae9f04f97
rexysiwabessy <rexysiwabessy@gmail.com>
2020-12-09 14:48:55 +0700

    rexy update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Auth;
+use DB;
+use View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('*', function ($view) {
+            if(Auth::check()) {
+                $schema = Auth::user()->schema.'.';
+                $menus = DB::table($schema.'usr_group_menu')
+                         ->join($schema.'usr_menu', 'usr_group_menu.menu_id', 'usr_menu.id')
+                         ->where('group_id', Auth::user()->group_id)->get();
+
+                View::share('menus', $menus);
+            }
+          });
+      
     }
 
     /**
