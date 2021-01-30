9d596bcb14e618eff2f5013cea8ede4358f27e1e
rexysiwabessy <rexysiwabessy@gmail.com>
2021-01-30 23:33:50 +0700

    rexy

--- app/Models/StkRepairHeader.php
+++ app/Models/StkRepairHeader.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class StkRepairHeader extends CompModel
 {
   protected $table = 'stk_repair_header';
+
+  public function stk_history_stok()
+  {
+    return $this->hasMany('App\Models\StkHistorySparePart', 'header_id');
+  }
 }
