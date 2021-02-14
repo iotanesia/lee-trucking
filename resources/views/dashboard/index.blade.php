9652def4fcbf353604bb4c52f32f06b70e658880
rexysiwabessy <rexysiwabessy@gmail.com>
2021-02-14 07:55:41 +0700

    rex

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -27,9 +27,9 @@
             <div class="col-xl-12">
                 <div class="card-body">
                     <div class="row">
-                        <div class="chart" style="margin-left:30px">
-                            <canvas id="bar-chart" width="400%" height="400%" class="chart-canvas"></canvas>
-                            <canvas id="pie-chart" width="400%" height="400%"></canvas>
+                        <div class="chart" style="margin-left:20px">
+                            <canvas id="bar-chart" width="350%" height="350%" class="chart-canvas"></canvas>
+                            <canvas id="pie-chart" width="350%" height="350%"></canvas>
                         </div>
                     </div>
                 </div>
