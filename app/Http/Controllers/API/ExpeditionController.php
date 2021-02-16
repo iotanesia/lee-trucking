3e1605490a2f0d818d7631fca7a42461e8950443
rexysiwabessy <rexysiwabessy@gmail.com>
2021-02-17 01:21:36 +0700

    re`

--- app/Http/Controllers/API/ExpeditionController.php
+++ app/Http/Controllers/API/ExpeditionController.php
@@ -277,10 +277,10 @@ class ExpeditionController extends Controller
             $row->otv_image = url('/uploads/expedition/'.$approvalCode['img']);
             $row->otv_nominal_kurang_bayar = $approvalCode['nominal_kurang_bayar'];
             $row->jenis_surat_jalan = substr($row->nomor_surat_jalan, 0, 2);
-            $row->services = $exStatusActivity['services'];
-            $row->service_charge = $exStatusActivity['service_charge'];
-            $row->status_destination = $exStatusActivity['status_destination'];
-            $row->new_address = $exStatusActivity['new_address'];
+            $row->services = $approvalCode['services'];
+            $row->service_charge = $approvalCode['service_charge'];
+            $row->status_destination = $approvalCode['status_destination'];
+            $row->new_address = $approvalCode['new_address'];
             $row->data_json = $row->toJson();
 
       }
