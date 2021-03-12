c8e4fb0e6ed7baea62c011b34d016cbfdee6f12b
rexysiwabessy <rexysiwabessy@gmail.com>
2021-03-12 23:09:34 +0700

    rexy

--- resources/views/expedition/new_ex/index.blade.php
+++ resources/views/expedition/new_ex/index.blade.php
@@ -343,9 +343,6 @@
 <script>
 
 $("document").ready(function() {
-    $("#search-data").val('asad');
-    alert($("#search-data").val());
-    document.getElementById("search-data").click();
     res = false;
     accessToken =  window.Laravel.api_token;
     $("#tujuan").select2({
