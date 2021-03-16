98c0160c8ab02e105b677e6a0f6480f025eb6984
Yudha <yudha.afuwwu.m@gmail.com>
2021-03-17 03:16:23 +0700

    repair truck

---
+++ public/js/truck-repair-report.js
@@ -0,0 +1,181 @@
+  
+  
+  $(document).ready(function() {  
+    // var accessToken =  window.Laravel.api_token;
+    var accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjQ5NTlhYjQ2ZWUwZmFjOWU1ZGYxYTdkMjY0NzE3NmFlZWViYTg5M2ExOTA4NjY0N2ZiNjhiZmUzYTk2MjNkYTk5YWE0YzM0Njg3NWMxY2QzIn0.eyJhdWQiOiIzIiwianRpIjoiNDk1OWFiNDZlZTBmYWM5ZTVkZjFhN2QyNjQ3MTc2YWVlZWJhODkzYTE5MDg2NjQ3ZmI2OGJmZTNhOTYyM2RhOTlhYTRjMzQ2ODc1YzFjZDMiLCJpYXQiOjE2MTUzMDU1OTksIm5iZiI6MTYxNTMwNTU5OSwiZXhwIjoxNjQ2ODQxNTk5LCJzdWIiOiIxMCIsInNjb3BlcyI6W119.X1minlba3vJY7FkBVY8Hi_ijTGdvmftNBk17863ItQbGhOUiAMCjK-TEHJst4PJMmZpBQdKa0GcpGwtOgYkCADS4uxYgG6FIuRCXsfetSx23TmF48PSlhMxyeG55i23aHwHUv-Ho7cKXwxOYDEOT10QBKGNYTs-TFzXMheajtxTJvgjGb7VzJCcA8tMn-n3DzKA9mT-ZU4CB9WSoCh4IjAisxRhOf2iC8IYxu_h-L5cC_R4jPirvTcOEtoPgQ752_O0XvDQDFoYH_Rdp0DOy3PkyhJrX3CL6HOAYwAI-ip2X2j4Z9-Hp0ddqFOAAszoauGrTxzgKZGus4VHcQ9NQjsfv7KrAlwLGpS0Zc-jWqfavzMz6OMNpevLc7c3OVVeWN4jUCrJTZCUnQMwZgr2rSN5yJLU20DjSpljN0N2NOot43hf83_K0e8iTsLFnwmLkyh7KezOtkMzHmBXSq1j2sVUs4jsZH-eOsh8Vs7aIFyxC4qIMV6h_mU8oFA1TaGhVyzzW_xLJgl9gGLRDONPP15AT6vmkFD14Ut6tJUbjpBV9FSshJ3JUTP-LjCKbAMao1TkEAOsrG2ag-V9R0pg-cym7Glok57_i_jJwEfbVSFXAD5v2sEo5rp0VVTM3x2hziuXH1q1UmGRg3HgqF0Iw2EVmuRNs7vgZXJwBJA3xFjc"
+   
+    var date = new Date(), y = date.getFullYear(), m = date.getMonth();
+    var firstDay = new Date(y, m, 1);
+    var lastDay = new Date(y, m+1, 0);
+
+    var startDate = '';
+    var endDate = '';
+    var detailId = '';
+  var table = $('#table-repair-truck-report').DataTable({
+    processing: true,
+    serverSide: true, autoWidth: true,
+    ajax: {
+      url: window.Laravel.app_url + "/api/report/get-repair-truck-list",
+      type: "GET",data: function (d) {
+        d.start_date = startDate;
+        d.end_date = endDate;
+      },
+      headers: {"Authorization": "Bearer " + accessToken},
+      crossDomain: true,
+    },
+    columns: [
+        {
+          "data": null, "sortable": false,
+            render: function (data, type, row, meta) {
+              return meta.row + meta.settings._iDisplayStart + 1;
+          }
+        },
+        {"data":"kode_repair"},
+        {"data":"truck_name"},
+        {
+          "data":"created_at", render: function (data, type, row, meta) {
+            return formatDate(data);
+          }
+        },
+        {"data":"total"},
+        {
+            "data": "id",
+            render: function (data, type, row) {
+              return '<a href="#" onclick="return openModalDetail(\'' + data + '\')" class="btn btn-success" style="padding-right:5px !important;padding-left:5px !important;margin-top:-10px !important;font-size:8pt !important;padding:3px !important">Detail</a>';
+            }
+        }
+    ],
+    scrollCollapse: true,
+    language: {
+        paginate: {
+            previous: '<i class="fas fa-angle-left"></i>',
+            next: '<i class="fas fa-angle-right"></i>'
+        }
+    }
+  });
+
+  function convertToRupiah(angka)
+  {
+    var rupiah = '';		
+    var angkarev = angka.toString().split('').reverse().join('');
+    for(var i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
+    return 'Rp. '+rupiah.split('',rupiah.length-1).reverse().join('');
+  }
+
+  $(function() {
+    $('input[name="dateRange"]').daterangepicker({
+      opens: 'right',
+      showDropdowns: true,
+    locale: {
+        format:'DD MMMM YYYY',
+        separator:' - ',
+        applyLabel: 'Pilih',
+        cancelLabel: 'Batal',
+        customRangeLabel:'Custom',
+        daysOfWeek:[
+            'Min',
+            'Sen',
+            'Sel',
+            'Rab',
+            'Kam',
+            'Jum',
+            'Sab'
+        ],
+        monthNames:[
+            'Januari',
+            'Februari',
+            'Maret',
+            'April',
+            'Mei',
+            'Juni',
+            'Juli',
+            'Agustus',
+            'September',
+            'Oktober',
+            'November',
+            'Desember'
+        ],
+        firstDay:'1'
+    }
+    },
+    function(start, end, label) {
+      startDate = start.format('YYYY-MM-DD');
+      endDate = end.format('YYYY-MM-DD');
+      $('#table-repair-truck-report').DataTable().ajax.reload();
+    });
+  $('input[name="dateRange"]').val('');
+  $('input[name="dateRange"]').attr("placeholder","Pilih Tanggal");
+  });
+
+  function formatDate(date) {
+    var d = new Date(date),
+        bulan = d.getMonth(),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+        switch(bulan) {
+          case 0: bulan = "Januari"; break;
+          case 1: bulan = "Februari"; break;
+          case 2: bulan = "Maret"; break;
+          case 3: bulan = "April"; break;
+          case 4: bulan = "Mei"; break;
+          case 5: bulan = "Juni"; break;
+          case 6: bulan = "Juli"; break;
+          case 7: bulan = "Agustus"; break;
+          case 8: bulan = "September"; break;
+          case 9: bulan = "Oktober"; break;
+          case 10: bulan = "November"; break;
+          case 11: bulan = "Desember"; break;
+         }
+
+ 
+    if (day.length < 2) 
+        day = '0' + day;
+    var result = [day, bulan, year].join(' ');
+    // console.log(result);
+    return result;
+  }
+
+  function formatDateReq(date) {
+    var d = new Date(date),
+    month = '' + (d.getMonth()+1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+  }
+  
+
+ $('#clearDate').click(function(e){
+    e.preventDefault();
+    $('input[name="dateRange"]').val('');
+    $('input[name="dateRange"]').attr("placeholder","Pilih Tanggal");
+    startDate = '';
+    endDate = '';   
+    $('#table-repair-truck-report').DataTable().ajax.reload();
+ })
+
+  $("#is-excel").click(function(e) {
+    e.preventDefault();
+    $("#tipeFile").val("excel");
+   
+    // return false;
+  });
+ 
+  $("#is-pdf").click(function(e) {
+    e.preventDefault();
+   
+    $("#tipeFile").val("pdf");
+    // return false;
+  });
+});
+
+   
+function openModalDetail(idHeader){
+    
+  }
