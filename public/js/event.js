f1ae12ffae6acebf843fe0ceec8f15be931ff112
rexysiwabessy <rexysiwabessy@gmail.com>
2021-03-17 00:02:32 +0700

    rexy

--- public/js/event.js
+++ public/js/event.js
@@ -435,7 +435,7 @@ function def(element) {
 
 function convertToRupiah(angka)
 {
-    if(angka !== null) {
+    if(angka !== null && angka !== undefined) {
         var rupiah = '';		
         var angkarev = angka.toString().split('').reverse().join('');
         for(var i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
