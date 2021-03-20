ce4ee9b5cb5720c8e25fb10b9eca608aae643500
rexysiwabessy <rexysiwabessy@gmail.com>
2021-03-20 22:22:38 +0700

    rexy

---
+++ public/js/bonusKenekRit.js
@@ -0,0 +1,253 @@
+$("document").ready(function(){
+    var accessToken =  window.Laravel.api_token;
+  
+    $.ajax({
+      url: window.Laravel.app_url + "/api/bonusDriverRit/get-list-kenek",
+      type: "GET",
+      dataType: "json",
+      headers: {"Authorization": "Bearer " + accessToken},
+      crossDomain: true,
+      beforeSend: function( xhr ) { 
+        $('.preloader').show();
+      },
+      success: function(data, textStatus, xhr) {
+        $('.preloader').hide();
+        successLoadbonusDriverRit(data);
+      },
+    });
+  
+    $("#btn-submit").click(function(){
+      var event = $("#bonusDriverRit-modal #btn-submit").attr("el-event");
+      var data = new FormData($("#bonusDriverRit-form")[0]);
+      data.append("_token", window.Laravel.csrfToken);
+  
+      $.ajax({
+        url: window.Laravel.app_url + "/api/bonusDriverRit/" + event + "",
+        type: "POST",
+        dataType: "json",
+        data: data,
+        processData: false,
+        contentType: false,
+        headers: {"Authorization": "Bearer " + accessToken},
+        crossDomain: true,
+        beforeSend: function( xhr ) {
+          $('.preloader').show();
+      },
+      success: function(datas, textStatus, xhr) {
+          $("#successModal").modal("show");
+          $("#bonusDriverRit-modal").modal("hide");
+          $('.preloader').hide();
+          document.getElementById("search-data").click();
+        },
+        error: function(datas, textStatus, xhr) {
+          $('.preloader').hide();
+          msgError = "";
+          for(var item in datas.responseJSON.errors) {
+            msgError += datas.responseJSON.errors[item][0] + "*";
+          }
+          alert(msgError);
+        }
+      });
+    });
+
+    $("#bonusDriverRit_status").select2({
+        placeholder:"Select Status"
+    });
+
+    $("#bonusDriverRit_parent").select2({
+        placeholder:"Select Parent"
+    });
+
+    $("#bonusDriverRit_category").select2({
+        placeholder:"Select Category"
+    });
+
+    $("#tahun-select").select2({
+        placeholder:"Select Tahun"
+
+    }).on('select2:select', function (e) {
+        var data = e.params.data;
+        var tahun = $("#tahun-select").val();
+        var bulan = $("#bulan-select").val();
+        
+        if(tahun != '' && bulan != '') {
+            var accessToken =  window.Laravel.api_token;
+  
+            $.ajax({
+                url: window.Laravel.app_url + "/api/bonusDriverRit/get-list?tahun="+tahun+"&bulan="+bulan+"",
+                type: "GET",
+                dataType: "json",
+                headers: {"Authorization": "Bearer " + accessToken},
+                crossDomain: true,
+                beforeSend: function( xhr ) { 
+                    $('.preloader').show();
+                },
+                success: function(data, textStatus, xhr) {
+                    $('.preloader').hide();
+                    successLoadbonusDriverRit(data);
+                },
+            });
+        }
+    });
+
+    $("#bulan-select").select2({
+        placeholder:"Select Bulan"
+    }).on('select2:select', function (e) {
+        var data = e.params.data;
+        var tahun = $("#tahun-select").val();
+        var bulan = $("#bulan-select").val();
+        
+        if(tahun != '' && bulan != '') {
+            var accessToken =  window.Laravel.api_token;
+  
+            $.ajax({
+                url: window.Laravel.app_url + "/api/bonusDriverRit/get-list?tahun="+tahun+"&bulan="+bulan+"",
+                type: "GET",
+                dataType: "json",
+                headers: {"Authorization": "Bearer " + accessToken},
+                crossDomain: true,
+                beforeSend: function( xhr ) { 
+                    $('.preloader').show();
+                },
+                success: function(data, textStatus, xhr) {
+                    $('.preloader').hide();
+                    successLoadbonusDriverRit(data);
+                },
+            });
+        }
+    });
+
+    $('#date-picker').daterangepicker({
+        singleDatePicker: true,
+        showDropdowns: true,
+        minDate: '06/01/2013',
+        maxDate: '06/30/2021',      
+        format: 'DD/MM/YYYY'
+      }).on('hide.daterangepicker', function (ev, picker) {
+        $('.table-condensed tbody tr:nth-child(2) td').click();
+        alert(picker.startDate.format('MM/YYYY'));
+      });
+  
+    $("#bonusDriverRit-modal").on("show.bs.modal", function(e) {
+      var invoker = $(e.relatedTarget);
+  
+      if(invoker.attr('el-event') == 'edit') {
+        var dataJSON = invoker.attr("data-json");
+        var dataJSON = JSON.parse(dataJSON);
+  
+        $("#bonusDriverRit-form").find("input[name=id]").val(dataJSON.id);
+        $("#bonusDriverRit-modal #btn-submit").attr("el-event", "edit");
+        $("#bonusDriverRit-form").find("textarea[name=content]").summernote("code", dataJSON.content);
+        
+        bindToForm($("#bonusDriverRit-modal"), dataJSON);
+        
+      } else {
+        $("#bonusDriverRit-form").find("input[name=id]").val(null);
+        $("#bonusDriverRit-modal #btn-submit").attr("el-event", "add");
+        $("#bonusDriverRit-form").find("textarea[name=content]").summernote("code", "");
+        resetForm("#bonusDriverRit-form");
+      }
+    });
+  });
+  
+  var successLoadbonusDriverRit = (function(responses, dataModel) {
+      
+    var tableRows = "";
+    var responses = responses.result.data == undefined ? responses : responses.result;
+  
+    for(var i = 0; i < responses.data.length; i++) {
+      id = responses.data[i].id;
+      kenek_name = responses.data[i].kenek_name;
+      rit_truck = responses.data[i].rit_truck;
+      truck = responses.data[i].truck;
+      total_rit = responses.data[i].total_rit;
+      bonus = responses.data[i].bonus;
+      reward_jenis = responses.data[i].reward_jenis;
+      data_json = responses.data[i].data_json;
+
+      tableRows += "<tr>" +
+                     "<td>"+ (i + 1) +"</td>"+
+                     "<td>"+ kenek_name +"</td>"+
+                     "<td>"+ truck +"</td>"+
+                     "<td>"+ def(total_rit) +"</td>"+
+                     "<td>"+ def(rit_truck) +"</td>"+
+                     "<td>"+ def(reward_jenis) +"</td>"+
+                     "<td>"+ convertToRupiah(parseInt(total_rit) * 10000) +"</td>"+
+                     "<td>"+  convertToRupiah(bonus)  +"</td>"+
+                     "<td>"+  convertToRupiah(parseInt(bonus) + (parseInt(total_rit) * 10000))  +"</td>"+
+                   "</tr>";
+    }
+  
+    if(!tableRows) {
+      tableRows += "<tr>" +
+                   "</tr>";
+    }
+  
+    $("#table-bonusDriverRit tbody").html(tableRows);
+    paginate(responses, 'bonusDriverRit');
+  
+    $(".preloader").hide();
+  
+    $("#select-provinsi").on("click", function() {
+        var id = $(this).val();
+  
+        $.ajax({
+          url: window.Laravel.app_url + "/api/daerah/get-kabupaten-by-idProv",
+          type: "GET",
+          dataType: "json",
+          data:"idProvinsi"+"="+id,
+          crossDomain: true,
+          beforeSend: function( xhr ) { 
+            $('.preloader').show();
+          },
+          success: function(data, textStatus, xhr) {
+              $("#select-kabupaten").html("");
+              $("#select-kecamatan").html("");
+              optData('#select-kabupaten', data, 'kabupaten');
+              $('.preloader').hide();
+          },
+        });
+    })
+  
+    $("#select-kabupaten").on("click", function() {
+      var id = $(this).val();
+  
+      $.ajax({
+        url: window.Laravel.app_url + "/api/daerah/get-kecamatan-by-idKab",
+        type: "GET",
+        dataType: "json",
+        data:"idKabupaten"+"="+id,
+        crossDomain: true,
+        beforeSend: function( xhr ) { 
+          $('.preloader').show();
+        },
+        success: function(data, textStatus, xhr) {
+            $("#select-kecamatan").html("");
+            optData('#select-kecamatan', data, 'kecamatan');
+            $('.preloader').hide();
+        },
+      });
+  })
+  
+    function optData(idSelect, res, title) {
+  
+      var opt = '';
+          opt += '<option value="0">--Select '+title+'--</option>';
+  
+      $.each(res.data, function( k, v) {
+          console.log(v.kabupaten);
+  
+          if(title == 'kabupaten') {
+              name = v.kabupaten;
+          
+          } else if(title == 'kecamatan') {
+              name = v.kecamatan;
+          }
+          
+          opt += '<option value="'+v.id+'"> '+name+' </option>';
+      });
+  
+      $(idSelect).html(opt);
+    }
+  });
+  
---
+++ resources/views/bonus-driver-rit/index-kenek.blade.php
@@ -0,0 +1,166 @@
+@extends('layouts/layouts')
+@section('content')
+<style>
+.table-condensed thead tr:nth-child(2),
+.table-condensed tbody {
+  display: none
+}
+.daterangepicker select.yearselect{
+    width:60%
+}  
+</style>
+    <div class="header bg-gradient-info pb-6">
+      <div class="container-fluid">
+        <div class="header-body">
+          <div class="row align-items-center py-4">
+            <div class="col-lg-6 col-7">
+              <h6 class="h2 text-white d-inline-block mb-0">{{$title}}</h6>
+              <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+                <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+                  <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
+                  <li class="breadcrumb-item active" aria-current="page">{{$title}}</li>
+                </ol>
+              </nav>
+            </div>
+          </div>
+          <!-- Card stats -->
+          <div class="row">
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Page content -->
+    <div class="container-fluid mt--6">
+      <div class="row">
+        <div class="col-xl-12">
+          <div class="card" id="bonusDriverRit">
+            <div class="card-header bg-transparent">
+              <div class="row align-items-center">
+                <div class="col">
+                  <h6 class="text-uppercase text-muted ls-1 mb-1">Data {{$title}}</h6>
+                  <h5 class="h3 mb-0">Table {{$title}}</h5>
+                  <br>
+                  <ul class="nav nav-pills nav-secondary nav-pills-no-bd nav-sm">
+                    <li class="nav-item submenu">
+                        <a class="nav-link" id="user-ad" href="{{url('bonus-driver-rit')}}" aria-selected="true">Driver</a>
+                    </li>
+                    <li class="nav-item submenu">
+                        <a class="nav-link active show" id="user-up" href="#" aria-selected="false">Kenek</a>
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-md-2">
+                    <select class="form-control m-1" id="tahun-select">
+                        <option value=""></option>
+                        @foreach($tahun as $val)
+                            <option value="{{$val['years']}}">{{$val['years']}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-md-2">
+                    <select class="form-control m-1" id="bulan-select">
+                        <option value=""></option>
+                        @foreach($bulan as $key => $val)
+                            <option value="{{$key}}">{{$val}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="navbar-search navbar-search-light form-inline mr-sm-3">
+                    <div class="form-group mb-0">
+                        <div class="input-group input-group-alternative input-group-merge">
+                            <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-search"></i></span>
+                            </div>
+                            <input type="text" id="btn-search-trigger" class="form-control has-primary" name="search_value" data-model="bonusDriverRit" placeholder="Search Key">
+                        </div>
+                    </div>
+                    <a type="button" class="input-group-text btn-sm btn-flat" style="display:none" request-url="{{ route('api-bonusDriverRit-get-list-kenek') }}" id="search-data" el-event="search-data" data-model="bonusDriverRit"><i class="fa fa-search"></i></a>
+                </div>
+              </div>
+            </div>
+            <div class="card-body">
+            <div class="tab-content">
+              <div id="ad" class="tab-pane in active">
+                <div class="table-responsive">
+                    <table class="table align-items-center table-flush table-striped" id="table-bonusDriverRit" data-model="bonusDriverRit" request-url="{{ route('api-bonusDriverRit-get-list-kenek') }}" on-success-load="successLoadbonusDriverRit">
+                        <thead class="bg-gradient-info text-white">
+                            <tr>
+                                <th>No</th>
+                                <th>Kenek</th>
+                                <th>Truck</th>
+                                <th>Total Rit Kenek</th>
+                                <th>Total Rit Truck</th>
+                                <th>Reward Jenis</th>
+                                <th>Pendapatan</th>
+                                <th>Bonus</th>
+                                <th>Total Pendapatan</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+                <div class="card-footer py-4">
+                    <nav aria-label="...">
+                        <ul class="pagination justify-content-end mb-0"></ul>
+                    </nav>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+      <!-- Footer -->
+      <footer class="footer pt-0">
+        <div class="row align-items-center justify-content-lg-between">
+          <div class="col-lg-6">
+            <div class="copyright text-center text-lg-left text-muted">
+              &copy; {{date('Y')}} <a href="http://liexpedition.com/" class="font-weight-bold ml-1" target="_blank">Lee-Tracking</a>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+              <li class="nav-item">
+                <a href="http://liexpedition.com/" class="nav-link" target="_blank">Contact</a>
+              </li>
+              <li class="nav-item">
+                <a href="http://liexpedition.com//presentation" class="nav-link" target="_blank">About Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+<div class="modal fade" id="bonusDriverRit-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Add bonusDriverRit</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <form role="form" id="bonusDriverRit-form">
+                <input type="hidden" name="id" id="id">
+                <div class="card-body">
+                    <div class="form-group">
+                    <label for="bonusDriverRit_name">bonusDriverRit Plat</label>
+                    <input type="text" class="form-control" name="bonusDriverRit_name" id="bonusDriverRit_name" placeholder="bonusDriverRit_name">
+                </div>
+                <div class="form-group">
+                    <label for="alamat">bonusDriverRit corporate asal</label>
+                    <textarea type="text" class="form-control" name="alamat" id="alamat" placeholder="alamat"> </textarea>
+                </div>
+            </form>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" el-event="add" id="btn-submit">Save changes</button>
+        </div>
+    </div>
+</div>
+<script src="{{asset('js/event.js')}}"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/momentjs/2.9.0/moment.min.js"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap.daterangepicker/1/daterangepicker.js"></script>
+<script src="{{asset('js/bonusKenekRit.js')}}"></script>
+@endsection
