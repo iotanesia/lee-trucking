7e22d13b93ce56d2898980dc40e68dd1ade891cd
rexy siwabessy <rexysiwabessy@gmail.com>
2021-09-28 13:23:56 +0700

    rexy

--- app/Models/BniDashBoadrd.php
+++ app/Models/BniDashBoadrd.php
@@ -29,7 +29,7 @@ class BniDashBoadrd extends CompModel
 
     static function getSlProdukChart() {
         try {
-            return BniDashBoadrd::select('unit', 'produk', DB::raw('COUNT(produk)'))->where('produk', '<>', '')->groupBy('unit', 'produk')->orderBy(DB::raw('COUNT(unit)'), 'DESC')->limit(5)->get()->toArray();
+            return BniDashBoadrd::select('unit', 'produk', DB::raw('COUNT(produk)'))->where('produk', '<>', '')->groupBy('unit', 'produk')->orderBy(DB::raw('COUNT(produk)'), 'DESC')->limit(5)->get()->toArray();
 
         } catch (\Throwable $th) {
             throw $th;
