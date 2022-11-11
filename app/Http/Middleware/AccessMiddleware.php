b71badf950a8931823caaab39c53a474692b8da8
rexy siwabessy <rexysiwabessy@gmail.com>
2022-11-12 01:25:26 +0700

    asd

--- app/Http/Middleware/AccessMiddleware.php
+++ app/Http/Middleware/AccessMiddleware.php
@@ -26,23 +26,23 @@ class AccessMiddleware
        try {
         
     $key = 'example_key';
-    $payload = [
-        'iss' => 'http://example.org',
-        'aud' => 'http://example.com',
-        'iat' => 1356999524,
-        'nbf' => 1357000000
-    ];
-    $jwt = JWT::encode($payload, $key, 'HS256');
-    dd($jwt);
-    $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
-    print_r($decoded);
-    $decoded_array = (array) $decoded;
-    JWT::$leeway = 60; // $leeway in seconds
-    $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+    // $payload = [
+    //     'iss' => 'http://example.org',
+    //     'aud' => 'http://example.com',
+    //     'iat' => 1356999524,
+    //     'nbf' => 1357000000
+    // ];
+    // $jwt = JWT::encode($payload, $key, 'HS256');
+    // $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+    // print_r($decoded);
+    // $decoded_array = (array) $decoded;
+    // JWT::$leeway = 60; // $leeway in seconds
+    // $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
 
 
             $token = $request->bearerToken();
-            dd($token);
+            // dd($token);
+            dd(JWT::decode($token, new Key($key, 'HS256')));
             if($token) {
                 try {
                     $credentials = Helper::decodeJwt($token);
