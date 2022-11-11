73b0c6833de5a179b9ad4087858bb3dfd053f125
rexy siwabessy <rexysiwabessy@gmail.com>
2022-11-12 01:18:03 +0700

    a

--- app/Helper.php
+++ app/Helper.php
@@ -13,25 +13,46 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 class Helper {
 
+
+    
+    // $key = 'example_key';
+    // $payload = [
+    //     'iss' => 'http://example.org',
+    //     'aud' => 'http://example.com',
+    //     'iat' => 1356999524,
+    //     'nbf' => 1357000000
+    // ];
+    // $jwt = JWT::encode($payload, $key, 'HS256');
+    // $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+    // print_r($decoded);
+    // $decoded_array = (array) $decoded;
+    // JWT::$leeway = 60; // $leeway in seconds
+    // $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+
+
+    
     static function createJwt($data = NULL, $is_refresh_token = FALSE) {
         $issued_at = time();
+        $key = 'example_key';
         $payload = [
-            'iss' => "sicana-2022", // Issuer of the token
+            'iss' => 'http://example.org', // Issuer of the token
+            'aud' => 'http://example.com',
             'sub' => $data, // Subject of the token
             'iat' => $issued_at, // Time when JWT was issued.
-            'exp' => $issued_at + 60*60*9999999999999999999999999999999999999
+            'exp' => $issued_at + 60*60*99999999999999999999999999999999
             // 'exp' => $is_refresh_token
             //     ?($issued_at + 60*60*24*30) // Waktu kadaluarsa 30 hari
             //     :($issued_at + 60*60*4) // Waktu kadaluarsa 1 jam
         ];
 
         JWT::$leeway = 60; // $leeway dalam detik
-        return JWT::encode($payload, 'leetrck','HS256');
+        return JWT::encode($payload, $key,'HS256');
     }
 
     static function decodeJwt($token) {
-        try {
-            return JWT::decode($token,new Key('leetrck', 'HS256'));
+        try {        
+            $key = 'example_key';
+            return JWT::decode($token,new Key($key, 'HS256'));
         } catch(\Throwable $e) {
             throw $e;
         }
